98bf8827d5b761740ee9e7cf145245472a222db6
prajjukorban <prajjukorban@gmail.com>
2024-10-29 18:36:47 +0530

    Portfolio update

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function clear(){
+  document.getElementById('name').innerText = "";
+  document.getElementById('email').innerText = "";
+  document.getElementById('message').innerText = "";
+}
+
+function change(url){
+window.location.href = url
+}
